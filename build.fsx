a2b412fb716fbf7449e6aab78215246158d52e2f
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2018-08-30 12:05:32 -0300

    Scaffolding FAKE build script

---
+++ build.fsx
@@ -0,0 +1,31 @@
+#r "paket:
+nuget Fake.Core.Target
+nuget Fake.DotNet.Cli"
+
+#load ".fake/build.fsx/intellisense.fsx"
+
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.Core.TargetOperators
+
+Target.create "Clean" (fun _ ->
+    !! "src/**/bin"
+    ++ "src/**/obj"
+    |> Shell.cleanDirs 
+)
+
+Target.create "Build" (fun _ ->
+    !! "src/**/*.*proj"
+    |> Seq.iter (DotNet.build id)
+)
+
+Target.create "All" ignore
+
+"Clean"
+  ==> "Build"
+  ==> "All"
+
+Target.runOrDefault "All"
