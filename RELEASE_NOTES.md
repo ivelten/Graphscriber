ce0017ab9e88955393911eb8fb3049630000bb90
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2019-04-17 22:24:26 -0300

    Updating release notes, packages and license

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,2 @@
+#### 0.0.1-beta - April 17 2019
+* Initial release
--- build.fsx
+++ build.fsx
@@ -1,6 +1,7 @@
 #r "paket:
 nuget Fake.Core.Target
 nuget Fake.DotNet.Cli
+nuget Fake.Core.ReleaseNotes
 nuget Fake.DotNet.Testing.Expecto
 nuget Fake.DotNet.Paket //"
 
@@ -41,6 +42,35 @@ Target.create "Build" (fun _ ->
 Target.create "Test" (fun _ ->
     !! "src/**/bin/Release/*/*Tests.dll"
     |> Expecto.run id)
+
+let project = "Graphscriber"
+let summary = "GraphQL over WebSocket implementation"
+let gitOwner = "ivelten"
+let gitHome = "https://github.com/" + gitOwner
+let gitName = "Graphscriber"
+let release = ReleaseNotes.load "RELEASE_NOTES.md"
+
+let pack id =
+    Shell.cleanDir <| sprintf "nuget/%s.%s" project id
+    Paket.pack(fun p ->
+        { p with
+            Version = release.NugetVersion
+            OutputPath = sprintf "nuget/%s.%s" project id
+            TemplateFile = sprintf "src/%s.%s/%s.%s.fsproj.paket.template" project id project id
+            IncludeReferencedProjects = true
+            MinimumFromLockFile = true
+        })
+
+let publish id =
+    pack id
+    Paket.push (fun options ->
+        { options with
+            WorkingDir = sprintf "nuget/%s.%s" project id
+            PublishUrl = "https://www.nuget.org/api/v2/package" })
+
+Target.create "PackAspNetCore" (fun _ -> pack "AspNetCore")
+
+Target.create "PublishAspNetCore" (fun _ -> publish "AspNetCore")
 
 Target.create "All" ignore
 
